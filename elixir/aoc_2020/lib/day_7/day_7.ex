ff4f89e22d4f7f46521b47492b7d3f200f568f4d
Markus Flambard <markus.flambard@youcal.com>
2020-12-07 09:00:04 +0100

    Day 7 part 2

--- elixir/aoc_2020/lib/day_7/day_7.ex
+++ elixir/aoc_2020/lib/day_7/day_7.ex
@@ -2,15 +2,17 @@ defmodule Day7 do
   def colors_that_may_contain(rules, color) do
     rules_that_contain_color =
       rules
-      |> Enum.filter(fn {bag_color, bags} -> 0 != bag_can_contain(bags, color) end)
+      |> Enum.filter(fn {_, bags} -> 0 != bag_can_contain(bags, color) end)
 
-    colors = Enum.map(rules_that_contain_color, fn {c, _} -> c end)
+    colors =
+      rules_that_contain_color
+      |> Enum.map(fn {c, _} -> c end)
+      |> MapSet.new()
 
-    result =
-      colors
-      |> Enum.map(fn c -> colors_that_may_contain(rules, c) end)
-
-    List.flatten(result ++ colors)
+    colors
+    |> Enum.map(fn c -> colors_that_may_contain(rules, c) end)
+    |> List.foldl(MapSet.new(), &MapSet.union/2)
+    |> MapSet.union(colors)
   end
 
   def bag_can_contain(bag, color) do
@@ -18,5 +20,11 @@ defmodule Day7 do
       nil -> 0
       {_, n} -> n
     end
+  end
+
+  def must_contain(rules, color) do
+    rules[color]
+    |> Enum.map(fn {n, c} -> n + n * must_contain(rules, c) end)
+    |> Enum.sum()
   end
 end
--- elixir/aoc_2020/test/day_7/day_7_test.exs
+++ elixir/aoc_2020/test/day_7/day_7_test.exs
@@ -19,17 +19,33 @@ defmodule Day7.Test do
     assert result == 4
   end
 
-  # @tag :skip
+  test "example 2" do
+    example_rules = %{
+      "light red" => [{1, "bright white"}, {2, "muted yellow"}],
+      "dark orange" => [{3, "bright white"}, {4, "muted yellow"}],
+      "bright white" => [{1, "shiny gold"}],
+      "muted yellow" => [{2, "shiny gold"}, {9, "faded blue"}],
+      "shiny gold" => [{1, "dark olive"}, {2, "vibrant plum"}],
+      "dark olive" => [{3, "faded blue"}, {4, "dotted black"}],
+      "vibrant plum" => [{5, "faded blue"}, {6, "dotted black"}],
+      "faded blue" => [],
+      "dotted black" => []
+    }
+
+    result = Day7.must_contain(example_rules, "shiny gold")
+    assert result == 32
+  end
+
+  @tag :skip
   test "real input 1" do
     colors = Day7.colors_that_may_contain(Input.rules, "shiny gold")
-    IO.inspect {:colors, colors}
-    result = Enum.count(MapSet.new(colors))
+    result = Enum.count(colors)
     IO.inspect({:day_6_answer_1, result})
   end
 
   @tag :skip
   test "real input 2" do
-    result = :todo
+    result = Day7.must_contain(Input.rules, "shiny gold")
     IO.inspect({:day_6_answer_2, result})
   end
 end
